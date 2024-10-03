e9db71b591e654839039b4b302caeeae81d8d84e
WEIQI ZHOU <wzhou255@wisc.edu>
2024-10-03 02:03:55 -0500

    Initial Commit

---
+++ AutoCalendar/AutoCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoCalendarApp.swift
+//  AutoCalendar
+//
+//  Created by 周玮琦 on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
